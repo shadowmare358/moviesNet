737b261c4bd2f560347784c25f69183158abd166
Mike <shadowmare@windowslive.com>
2020-01-14 19:01:39 +0100

    Dodaj pliki projektów.

---
+++ mvcapp/Controllers/InfoController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace mvcapp.Controllers
+{
+    public class InfoController : Controller
+    {
+        public string Info()
+        {
+            return "Info from info controller";
+        }
+        public IActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewData["Message"] = "Hello " + name;
+            ViewData["NumTimes"] = numTimes;
+            return View();
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ mvcapp/Data/MvcMovieContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using mvcapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace mvcapp.Data
+{
+    public class MvcMovieContext : DbContext
+    {
+        public MvcMovieContext (DbContextOptions<MvcMovieContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Movie> Movie { get; set; }
+    }
+}
